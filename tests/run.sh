a6c881b9587cd2077e3f14aac4fcac792f6ee8f3
hirokuma <hiro9ma@gmail.com>
2024-09-25 10:08:05 +0900

    revise test result output

--- tests/run.sh
+++ tests/run.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
 
 TEST_ARGS=$@
+TEST_LOGFILE=test.log
 LCOV_INFO=build/lcov.info
 LCOV_OUTPUT=build/lcov-output
 
+test_and_lcov()
+{
+  DIR=$1
+  BAK=`pwd`
+  cd ${DIR}
+  # rm -rf build/CMakeFiles/app.dir/src/*.gc* \
+  #   build/CMakeFiles/app.dir/runner/*.gc*
+  west build -b native_sim_64 -t run ${TEST_ARGS}
+  cnt=`find build/CMakeFiles/app.dir/src -name "*.gcno" | wc -l`
+  if [ $cnt -ne 0 ]; then
+    lcov --quiet --capture --directory build/CMakeFiles/app.dir/src/ --output-file ${LCOV_INFO} 2> /dev/null
+    genhtml --quiet ${LCOV_INFO} --output-directory ${LCOV_OUTPUT} --show-details --legend 2> /dev/null
+    if [ -f ${DIR}/${LCOV_OUTPUT}/index.html ]; then
+      echo "\n======\nCOVERAGE: ${DIR}/${LCOV_OUTPUT}/index.html\n=====\n"
+    fi
+  fi
+  cd ${BAK}
+}
+
 exec_test()
 {
-    DIR=$1
-    BAK=`pwd`
-    cd ${DIR}
-    rm -rf build/CMakeFiles/app.dir/src/*.gc* \
-      build/CMakeFiles/app.dir/runner/*.gc*
-    west build -b native_sim_64 -t run ${TEST_ARGS}
-    cnt=`find build/CMakeFiles/app.dir/src -name "*.gcno" | wc -l`
-    if [ $cnt -ne 0 ]; then
-      lcov --capture --directory build/CMakeFiles/app.dir/src/ --output-file ${LCOV_INFO} > /dev/null 2>&1
-      genhtml ${LCOV_INFO} --output-directory ${LCOV_OUTPUT} --show-details --legend > /dev/null 2>&1
-      if [ -f ${DIR}/${LCOV_OUTPUT}/index.html ]; then
-        echo "\n======\nCOVERAGE: ${DIR}/${LCOV_OUTPUT}/index.html\n=====\n"
-      fi
-    fi
-    cd ${BAK}
+  test_and_lcov $1 >> ${TEST_LOGFILE}
+  sed -i -e "s|\*\*\* Booting nRF Connect SDK.*$|=====\n===== TEST: $1 =====\n=====|g" ${TEST_LOGFILE}
 }
+
+rm -f ${TEST_LOGFILE}
 
 exec_test drivers/led
 exec_test services/hls
+
+# test result
+sed -n '/^=====$/,/^PROJECT EXECUTION SUCCESSFUL$/p' ${TEST_LOGFILE}
